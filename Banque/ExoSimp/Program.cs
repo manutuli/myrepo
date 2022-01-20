715a7d7d869911669b042d373ff68a8a4425c24d
manutuli <97235754+manutuli@users.noreply.github.com>
2022-01-20 23:02:30 +0100

    projets c sharp studio

---
+++ Banque/ExoSimp/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExoSimp
+{
+    internal class Program
+    {
+        public enum AppState
+        {
+            Connexion = 101,
+            MainMenu = 102,
+            // Main menu options :
+            CheckPosNegZer = 1,
+            CheckCase = 2,
+            CheckOddEven = 3,
+            LargestNum = 4,
+            LargestNumCond = 5,
+            LargestNumThree = 6,
+            CheckLeapYear = 7,
+            CheckVowels = 8,
+        }
+        /*
+       C# Program to Add Two Integers.
+       C# Program to Swap Values of Two Variables.
+       C# Program to Multiply two Floating Point Numbers
+       C# Program to perform all arithmetic operations
+       C# Program to convert feet to meter
+       C# Program to convert celcius to farenheit
+       C# Program to convert farenheit to celcius
+       C# Program to find the Size of data types
+       C# Program to Print ASCII Value
+       C# Program to Calculate Area of Circle
+       C# Program to Calculate Area of Square
+       C# Program to Calculate Area of Rectangle
+       C# Program to convert days to years, weeks and days
+       */
+        //C# program to check number is positive, negative or zero.
+        public static string NumCheck(int arg)
+        {
+            if(arg == 0)
+            {
+                return $"The number \"{arg}\" is equal to zero !";
+            }
+            else if (arg > 1)
+            {
+                return $"The number \"{arg}\" is positive !";
+            }
+            else
+            {
+                return $"The number \"{arg}\" is negative !";
+            }
+
+        }
+        //C# program to check uppercase or lowercase alphabets.
+        public static string CaseCheck(string arg)
+        {
+            Regex _rgxUpper = new Regex(@"([A-Z])");
+            Regex _rgxLower= new Regex(@"([a-z])");
+            if (_rgxUpper.IsMatch(arg))
+            {
+                return "Cet alphabet contient des lettres majuscules.";
+            }
+            else if (_rgxLower.IsMatch(arg))
+            {
+                return "Cet alphabet contient des lettres minuscules.";
+            }
+            else { return "Cet alphabet ne contient ni majuscules, ni minuscules."; }
+        }
+        /*
+        C# program to check entered character vowel or consonant.
+        C# program to check whether a character is alphabet, digit or special character.
+        C# program to print day name of week.
+        C# program to accept two integers and check whether they are equal or not.
+        C# program to detrermine a candidate\’s age is eligible for casting the vote or not.
+        C# program to find the eligibility of admission for an engineering course based on the criteria.
+        C# program to calculate the total marks, percentage and division of student.
+        C# program to enter month number and print number of days in month.
+        C# program to count total number of notes in entered amount.
+        C# program to check whether a triangle can be formed by the given value for the angles.
+        */
+        public static void Main(string[] args)
+        {
+            while (_isRunning)
+            {
+                switch (_appstate)
+                {
+
+                }
+            }
+        }
+        //C# Program to check whether an integer entered by the user is odd or even
+        public static string OddNum()
+        {
+            int i = int.Parse(Console.ReadLine());
+            if ((i % 2) == 0)
+            {
+                return $"Yes, {i} is even !";
+            }
+            else
+            {
+                return $"No, {i} is not even !";
+            }
+        }
+
+        //C# Program to find the largest number among three number.
+        public static int LargestNum(int num1, int num2, int num3)
+        {
+
+            int[] ar = new int[3] {num1, num2, num3 };
+            Array.Sort(ar);
+            return ar[3];
+            /*
+            int[] ar = new int[3] {num1, num2, num3 };
+            int[] tb = new int[3] ;
+            foreach(int item in ar)
+            {
+                for (int i = 0; i < ar.Length; i++)
+                {
+                    int x = ar[i] <= ar[i+1] ? new int { ar[i+1] } : new int { ar[i] };
+                    tb = new int { x };
+                }
+            }
+             */
+        }
+
+        //C# Program to Find the Largest Number using Conditional Operator.
+        public static int LargestCond(int num1, int num2, int num3)
+        {
+            int temp;
+            int x ;
+            int y ;
+            temp = num1 > num2 ? num1 : num2;
+            x = temp > num3 ? temp : num3;
+            return y = x > num1 ? x : num1;        
+        }
+
+        //C# Program to find the Largest among Three Variables using Nested if.
+        public static int LargestThree(int num1, int num2, int num3)
+        {
+            if (num1 != num2 && num1 != num3 && num2 != num3)
+            {
+                if (num1 < num3 && num2 < num3)
+                {
+                    return num3;
+                }
+                else if (num2 > num1 && num2 > num3){
+                    return num2;
+                }else{
+                    return num1;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //C# program to check leap year using conditional Operator.
+        public static string LeapYear(int _year)
+        {
+
+            if ((_year % 4 == 0) && (_year % 100 != 0) || (_year % 400 == 0))
+            {
+                return $"The year {_year} is Leap year.";
+            }
+            else
+            {
+                return $"{_year} is Not Leap year.";
+            }
+        }
+
+        //C# Program to Check whether an alphabet is a vowel or not.
+        public static string CheckVowels()
+        {
+            Regex _rgxVowel = new Regex(@"([aoiueyAOIUEY])");
+            if (_rgxVowel.IsMatch(Console.ReadLine()))
+            {
+                return "Ce texte contient des voyelles !";
+            }
+            else
+            {
+                return "Ce texte ne contient aucune voyelle, désolé !";
+            }
+        }
+
+    }
+}
---
+++ Banque/Program.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+	public enum AppState
+	{
+		Connexion = 101,
+		MainMenu = 102,
+		// main menu options :
+		DisplayBalance = 1,
+		WithDraw = 2,
+		Credit = 3,
+		ChangePin = 4,
+		Home = 5,
+		Quit = 6,
+	}
+
+	public class MyAccount
+	{
+		public decimal m_balance;
+		public string m_pin;
+
+		public MyAccount(decimal b, string p)
+		{
+			m_balance = b;
+			m_pin = p;
+		}
+	}
+
+
+	internal class Program
+	{
+		#region Fields
+
+		private static Dictionary<string, MyAccount> _bankData = new Dictionary<string, MyAccount>()
+		{
+			{ "0001", new MyAccount(100,"987")},
+			{ "123", new MyAccount(99999,"123")},
+		};
+
+		private static string _str;
+		private static string _currentCard;
+		private static MyAccount _currentAccount;
+
+		// Regex à tester					---- CONTINUER avec exclusion de tout ce qui n'est pas un nombre
+		private static Regex _rgxAccount = new Regex(@"([0-9])");
+
+		// Regex à tester					---- CONTINUER avec exclusion de tout ce qui n'est pas un nombre
+		private static Regex _rgxPin = new Regex(@"([0-9])");
+
+		// état de l'application
+		private static AppState _appState = AppState.Home;
+
+		// App is running
+		private static bool _isRunning = true;
+
+		#endregion
+
+
+
+		#region Methods
+
+		static void Main(string[] args)
+		{
+			while (_isRunning)
+			{
+				switch (_appState)
+				{
+					case AppState.Home:
+						DisplayHeader("Bienvenue dans le DAB CléManuAle");
+						_appState = AppState.Connexion;
+						break;
+					case AppState.Connexion:
+						Connexion();
+						break;
+					case AppState.MainMenu:
+						DisplayHeader("Menu principal");
+						MainMenu();
+						break;
+					case AppState.DisplayBalance:
+						DisplayHeader("Solde");
+						DisplayBalance();
+						break;
+					case AppState.WithDraw:
+						DisplayHeader("Retrait");
+						WithDraw();
+						break;
+					case AppState.Credit:
+						DisplayHeader("Depôt");
+						Credit();
+						break;
+					case AppState.ChangePin:
+						DisplayHeader("Changer le PIN");
+						ChangePin();
+						break;
+					case AppState.Quit:
+						DisplayHeader("Quitter");
+						QuitApp();
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		private static void DisplayHeader(string title)
+		{
+			Console.Clear();
+			Console.WriteLine("------------------");
+			Console.WriteLine(title);
+			Console.WriteLine("------------------");
+		}
+
+		private static void ControlBackMenu()
+		{
+			if (Console.ReadKey().Key == ConsoleKey.Enter)
+			{
+				_appState = AppState.MainMenu;
+			}
+		}
+
+		private static void Connexion()
+		{
+			int step = 0;
+
+			// Test du numéro de carte
+			while (step == 0)
+			{
+				Console.WriteLine("Entrer un numéro de carte valide. Entrer \"Annuler\" pour annuler.");
+				_str = Console.ReadLine();
+				if (_rgxAccount.IsMatch(_str) && _bankData.ContainsKey(_str))
+				{
+					step = 1;
+					break;
+				}
+				else if (_str == "Annuler")
+				{
+					_appState = AppState.Home;
+					return;
+				}
+			}
+
+			_currentCard = _str;
+
+			while (step == 1)
+			{
+				Console.WriteLine("Entrer un PIN valide. Entrer \"Annuler\" pour annuler.");
+				_str = Console.ReadLine();
+				if (_rgxPin.IsMatch(_str) && _str == _bankData[_currentCard].m_pin)
+				{
+					step = 2;
+					break;
+				}
+				else if (_str == "Annuler")
+				{
+					_appState = AppState.Home;
+					return;
+				}
+			}
+
+			Console.WriteLine("Compte trouvé et PIN correspondant.");
+			_currentAccount = _bankData[_currentCard];
+			_appState = AppState.MainMenu;
+		}
+
+		private static void MainMenu()
+		{
+			Console.WriteLine(@"Main menu :
+	1 : afficher solde
+	2 : retrait
+	3 : dépôt
+	4 : changer PIN
+	5 : déconnecter
+	6 : quitter
+			");
+
+			int result = 0;
+			_appState = AppState.Quit;
+			bool isChoiceOk = false;
+			while (!isChoiceOk)
+			{
+				Console.WriteLine("Choisissez une option. ");
+				_str = Console.ReadLine();
+				
+				if (int.TryParse(_str, out result) && result >= 1 && result == (int)_appState)
+				{
+					isChoiceOk = true;
+				}
+			}
+
+			Console.WriteLine($"Menu choisi {result}");
+
+			_appState = (AppState)Enum.Parse(typeof(AppState), _str);
+
+		}
+
+		private static void DisplayBalance()
+		{
+			Console.WriteLine($"Solde = {_currentAccount.m_balance}. Appuyer sur Entrée pour revenir au menu.");
+			ControlBackMenu();
+		}
+
+		private static void WithDraw()
+		{
+			if (_currentAccount.m_balance == 0)
+			{
+				Console.WriteLine("Retrait impossible. Votre compte est à sec.");
+				_appState = AppState.MainMenu;
+				return;
+			}
+
+			Console.WriteLine($"Vous avez {_currentAccount.m_balance}. Combien voulez-vous retirer ?");
+
+			decimal result;
+
+			do
+			{
+				Console.WriteLine("Entrer un montant valide et inférieur au solde.");
+				_str = Console.ReadLine();
+			} while (!decimal.TryParse(_str, out result) || result > _currentAccount.m_balance || result <= 0);
+
+			_currentAccount.m_balance -= result;
+
+			Console.WriteLine($"Vous avez retiré {result}. " +
+				$"Il reste {_currentAccount.m_balance} sur le compte." +
+				$" Appuyer sur Entrée pour revenir au menu.");
+			ControlBackMenu();
+		}
+
+		private static void Credit()
+		{
+			Console.WriteLine($"Vous avez {_currentAccount.m_balance}. Combien voulez-vous déposer ?");
+
+			decimal result;
+
+			do
+			{
+				Console.WriteLine("Entrer un montant valide.");
+				_str = Console.ReadLine();
+			} while (!decimal.TryParse(_str, out result) || result <= 0);
+
+			_currentAccount.m_balance += result;
+
+			Console.WriteLine($"Vous avez crédité {result}. Votre compte est de {_currentAccount.m_balance}." +
+				$" Appuyer sur Entrée pour revenir au menu.");
+			ControlBackMenu();
+		}
+
+		private static void ChangePin()
+		{
+			Console.WriteLine($"Votre PIN est {_currentAccount.m_pin}");
+			Console.WriteLine("Le numéro est uniquement composé de 3 chiffres.");
+
+			do
+			{
+				Console.WriteLine("Entrez un nouveau PIN valide.");
+				_str = Console.ReadLine();
+			} while (!_rgxPin.IsMatch(_str));
+
+			_currentAccount.m_pin = _str;
+
+			Console.WriteLine("Votre PIN a été modifié. Appuyer sur Entrée pour revenir au menu.");
+			ControlBackMenu();
+		}
+
+		private static void QuitApp()
+		{
+			Console.WriteLine("Merci d'avoir utilisé cette borne.");
+			Environment.Exit(0);
+		}
+
+		#endregion
+	}
+}
